e585760b2af3cc98d519abb8d043f78f2edc8f2b
Jack Lin <farwill@gmail.com>
2019-02-21 21:24:31 +0800

    Updates

---
+++ precache-manifest.f65dd228cdbc91c2e8dec4d46d156992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1453c2448bc2c03d04a3",
+    "url": "/static/js/main.1453c244.chunk.js"
+  },
+  {
+    "revision": "ee9139a0dd5bf69c0a54",
+    "url": "/static/js/2.ee9139a0.chunk.js"
+  },
+  {
+    "revision": "1453c2448bc2c03d04a3",
+    "url": "/static/css/main.99b62b5b.chunk.css"
+  },
+  {
+    "revision": "865c951affca0717aecf09aed1e965bc",
+    "url": "/index.html"
+  }
+];
