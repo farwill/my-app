2a2e418ce13022606a88bcfae97032797c43f992
Jack Lin <farwill@gmail.com>
2019-08-10 10:12:25 +0800

    npm audit fix

---
+++ build/precache-manifest.ffb008aff9ba6124f9b36fbdfe649b87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f573e5b6877ab4c7ad2da7ec47de5b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2a2421f082f2beadaf28",
+    "url": "/my-app/static/css/main.65ae8676.chunk.css"
+  },
+  {
+    "revision": "c2f0aa6ccdeec4d81294",
+    "url": "/my-app/static/js/2.0f44849a.chunk.js"
+  },
+  {
+    "revision": "2a2421f082f2beadaf28",
+    "url": "/my-app/static/js/main.ef9c3432.chunk.js"
+  },
+  {
+    "revision": "52bb0c224796ebc91f98",
+    "url": "/my-app/static/js/runtime~main.3b25cb5d.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.8074f8bd760ce6c99d979e031132529a.js"
+  "/my-app/precache-manifest.ffb008aff9ba6124f9b36fbdfe649b87.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
